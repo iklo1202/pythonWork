e3c04c3c44ea5e739ff448588acd9c62c5ba7ecd
kent <iklo120277@gmail.com>
2020-02-26 17:46:35 +0800

    The second edition of python reading files

--- Reading Files/Reading files_try_except.py
+++ Reading Files/Reading files_try_except.py
@@ -1,15 +1,14 @@
-import os
-
-if os.path.exists('sketch.txt'):
+try:
     data = open('sketch.txt')
     for each_line in data:
-        if  not each_line.find(':') == -1:
+        try:
             (role, line_spoken) = each_line.split(':', 1)
             print(role, end='')
             print(' said: ', end='')
             print(line_spoken, end='')
-
+        except:
+            pass
     
     data.close()
-else:
+except IOError:
     print('The data file is missing!')
